fd709d9787b4c865b0acde379224ed3a25cae086
Syed Aman <sna274@nyu.edu>
2017-02-26 14:38:09 -0600

    create model for account

--- up-and-going/phone-store/controller.js
+++ up-and-going/phone-store/controller.js
@@ -1,8 +1,20 @@
 // used to calculate final total
 const TAX_RATE = 0.08;
 
-// user credentials
-var username;
-var firstName;
-var lastName;
-let bankBalance;
+// account model
+var account = {
+  username: 'string',
+  firstName: 'string',
+  lastName: 'string',
+  bankBalance: 'number',
+  fullName: () => {
+    return this.firstName + '' + this.lastName
+  },
+}
+
+createAccount() {
+  var username;
+  var firstName;
+  var lastName;
+  let bankBalance;
+}
